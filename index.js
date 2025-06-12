8e92f660c98d3649ca140e482e6a445e975c3afc
Alex-Spartan <hrushikeshvibhute@gmail.comm>
2025-06-12 15:27:17 +0530

    cors error resolve

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = 3000;
 const router = Router();
 app.use(cors({
     credentials: true,
-    origin: "https://gotrip-virid.vercel.app/"
+    origin: "https://gotrip-virid.vercel.app"
 }));
 app.use(express.json());
 app.use(cookieParser());
